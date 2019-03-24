2fe3edda7fe15b73652ea230994df876bbb4caf1
so0046iv <48401987+so0046iv@users.noreply.github.com>
2019-03-25 00:13:46 +0900

    Add files via upload

---
+++ user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15}
+  validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+
+  has_secure_password
+  validates :password, :password_confirmation, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i}
+end
